c33b2983b974e21361b42178731632fd9542eb56
Shivam Chavan <144063863+shivam-chavan-05@users.noreply.github.com>
2025-12-22 17:07:11 -0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__logo">
         <img src={logo} alt="logo" />
-        <h5>MyPizza</h5>
-        <p>Best Pizzas in town, try it out!</p>
+        <h5>MyOrders</h5>
+        <p>Ambrosia, try it out!</p>
       </div>
       <div>
         <h5 className="footer__title mb-3">Delivery Time</h5>
